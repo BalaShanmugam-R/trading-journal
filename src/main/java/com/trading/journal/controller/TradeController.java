43d06e6fa0f0038ab56947b296cc3633fd8ac9b2
Balashanmugam <balashan.1999@gmail.com>
2026-03-07 03:05:08 +0530

    update API added and code cleanup

--- src/main/java/com/trading/journal/controller/TradeController.java
+++ src/main/java/com/trading/journal/controller/TradeController.java
@@ -2,7 +2,6 @@ package com.trading.journal.controller;
 
 import com.trading.journal.model.TradeRequest;
 import com.trading.journal.model.TradeResponseList;
-import com.trading.journal.model.TradeUpdateRequest;
 import com.trading.journal.service.TradeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Tag(name = "trade-journal-collection-api", description = "Trade Journal Collection API")
 @Validated
@@ -34,6 +31,12 @@ public class TradeController {
     public ResponseEntity<String> addTrade(@RequestBody TradeRequest tradeRequest) {
         String response = tradeService.addTrade(tradeRequest);
         return ResponseEntity.ok().body(response);
+    }
+
+    @Operation(summary = "Endpoint to update editable fields in record", description = "This endpoint will be used to modify specific trade record.", operationId = "trade-journal")
+    @PutMapping
+    public String updateTrade(@Validated @RequestBody TradeRequest tradeRequest) {
+        return tradeService.updateTrade(tradeRequest);
     }
 
     @Operation(summary = "Endpoint to flush all trade records at once", description = "This endpoint will be used to delete all trade.", operationId = "trade-journal")
--- src/main/java/com/trading/journal/repository/TradeRecordRepository.java
+++ src/main/java/com/trading/journal/repository/TradeRecordRepository.java
@@ -19,51 +19,40 @@ public interface TradeRecordRepository extends JpaRepository<TradeRecordEntity, 
     void truncateTable();
 
     @Query(value = """
-    SELECT DISTINCT
-        r.exchange, r.symbol, r.instrument, r.order_type, r.quantity, 
-        r.buy_price, r.sell_price, r.strike_price, r.option_type,
-        r.entry_value, r.exit_value, r.stop_loss_value, r.target_value, 
-        r.trade_date, r.expiry_date,
-        s.profit, s.loss,
-        s.status, s.risk_reward_ratio, s.captured_points
-    FROM trade_records r 
-    LEFT JOIN trade_summary s ON 
-        r.strike_price = s.strike_price 
-        AND r.option_type = s.option_type 
-        AND r.entry_value = s.entry_value 
-        AND r.exit_value = s.exit_value
-    ORDER BY r.trade_date DESC
-    """, nativeQuery = true)
+            SELECT DISTINCT
+                r.exchange, r.symbol, r.instrument, r.order_type, r.quantity, 
+                r.buy_price, r.sell_price, r.strike_price, r.option_type,
+                r.entry_value, r.exit_value, r.stop_loss_value, r.target_value, 
+                r.trade_date, r.expiry_date,
+                s.profit, s.loss,
+                s.status, s.risk_reward_ratio, s.captured_points
+            FROM trade_records r 
+            LEFT JOIN trade_summary s ON 
+                r.strike_price = s.strike_price 
+                AND r.option_type = s.option_type 
+                AND r.entry_value = s.entry_value 
+                AND r.exit_value = s.exit_value
+            ORDER BY r.trade_date DESC
+            """, nativeQuery = true)
     List<Object[]> findAllTradesWithSummary();
 
     @Query(value = """
-    SELECT EXISTS (
-        SELECT 1 FROM trade_records r 
-        WHERE r.strike_price = :strikePrice 
-          AND r.option_type = :optionType 
-          AND r.entry_value = :entryValue 
-          AND r.exit_value = :exitValue
-    )
-    """, nativeQuery = true)
-    boolean findByTradeKey(
-            @Param("strikePrice") Integer strikePrice,
-            @Param("optionType") String optionType,
-            @Param("entryValue") Double entryValue,
-            @Param("exitValue") Double exitValue
-    );
+            SELECT EXISTS (
+                SELECT 1 FROM trade_records r 
+                WHERE r.strike_price = :strikePrice 
+                  AND r.option_type = :optionType 
+                  AND r.entry_value = :entryValue 
+            )
+            """, nativeQuery = true)
+    boolean findExistByTradeKey(@Param("strikePrice") Integer strikePrice, @Param("optionType") String optionType, @Param("entryValue") Double entryValue);
 
     @Query(value = """
-        DELETE FROM trade_records r 
-        WHERE r.strike_price = :strikePrice 
-          AND r.option_type = :optionType 
-          AND r.entry_value = :entryValue 
-          AND r.exit_value = :exitValue
-    """, nativeQuery = true)
-    boolean deleteByTradeKey(
-            @Param("strikePrice") Integer strikePrice,
-            @Param("optionType") String optionType,
-            @Param("entryValue") Double entryValue,
-            @Param("exitValue") Double exitValue
-    );
+                SELECT * FROM trade_records r 
+                WHERE r.strike_price = :strikePrice 
+                  AND r.option_type = :optionType 
+                  AND r.entry_value = :entryValue 
+            """, nativeQuery = true)
+    TradeRecordEntity findByTradeKey(@Param("strikePrice") Integer strikePrice, @Param("optionType") String optionType, @Param("entryValue") Double entryValue);
 
+    void deleteByStrikePriceAndOptionTypeAndEntryValue(Integer strikePrice, String optionType, Double entryValue);
 }
